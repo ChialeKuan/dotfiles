0a46783424876543929547005243d74701aecb14
chialekuan <chialekuan@qq.com>
2019-10-22 18:02:51 +0800

    20191022

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-apt = "apt"
-if [[ -f /usr/local/bin/brew ]]; then
-    cmd="brew"
+apt = "brew"
+if [[ -f /bin/apt ]]; then
+    cmd="apt"
 fi
 
 if [ `command -v zsh` ];
 then
     echo "zsh 已经安装"
 else
-    sudo apt update;
-    sudo apt install zsh;
+    sudo ${apt} update;
+    sudo ${apt} install zsh;
     sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 git pull
 cp ./aliases.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/
-source ~/.zshrc
 
