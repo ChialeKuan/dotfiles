4ea67a2f07b15c3e4034e4c416ec0c6983f9c9c6
chialekuan <chialekuan@qq.com>
2019-10-22 14:11:16 +0800

    20191022

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [ `command -v zsh` ];
+then
+    echo "zsh 已经安装"
+else
+    sudo apt update;
+    sudo apt upgrade;
+    sudo apt install zsh;
+    sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+git pull
 cp ./.alias ~
 source ~/.zshrc
 
---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+if [ `command -v java` ];then
+     echo 'java 已经安装'
+ fi
