357079e90232adde9c0c8530c69c35f4269e2204
chialekuan <chialekuan@qq.com>
2019-10-22 14:20:22 +0800

    20191022

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp ~/.aliases .
+cp ~/.zshrc .
 git add .
 git commit -m $(date "+%Y%m%d")
 git push
--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,11 @@ else
     sudo apt upgrade;
     sudo apt install zsh;
     sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
 
 git pull
-cp ./.alias ~
+cp ./.aliases ~
+cp ./.zshrc ~
 source ~/.zshrc
 
